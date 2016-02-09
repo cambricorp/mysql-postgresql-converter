6b6efe121f97b7ef5000ce089ecec8bc68d5639d
Devan Penner-Woelk <tycoonmaster@gmail.com>
2016-02-09 13:18:55 -0500

    Modify line prior to output
    Remove duplicate line output

--- db_converter.py
+++ db_converter.py
@@ -85,8 +85,9 @@ def parse(input_filename, output_filename):
                 creation_lines = []
             # Inserting data into a table?
             elif line.startswith("INSERT INTO"):
-                output.write(re.sub(r"([^'])'0000-00-00'", r"\1NULL", line.encode("utf8")) + "\n")
-                output.write(re.sub(r"([^'])'0000-00-00 00:00:00'", r"\1NULL", line.encode("utf8")) + "\n")
+                line = re.sub(r"([^'])'0000-00-00 00:00:00'", r"\1NULL", line)
+                line = re.sub(r"([^'])'0000-00-00'", r"\1NULL", line)
+                output.write(line.encode("utf8") + "\n")
                 num_inserts += 1
             # ???
             else:
