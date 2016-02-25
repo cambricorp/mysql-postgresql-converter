27c3f33d004a2549914149a1cd36453af148232e
Devan Penner-Woelk <tycoonmaster@gmail.com>
2016-02-25 16:20:16 -0500

    Typo

--- db_converter.py
+++ db_converter.py
@@ -207,7 +207,7 @@ def parse(input_filename, output_filename):
                         cast_lines.append("ALTER TABLE \"%s\" ALTER COLUMN \"%s\" SET DEFAULT %s" % (current_table, name, final_default))
                 # ID fields should be {singular_table_name}_id
                 primary_name = "%s_id" % (current_table.rstrip("s"))
-                if (name == primary_name or (current_table, name) in primary_names) and set_sequence is True:
+                if (name == primary_name or [current_table, name] in primary_names) and set_sequence is True:
                     sequence_lines.append("DROP SEQUENCE IF EXISTS %s_id_seq" % (current_table))
                     sequence_lines.append("CREATE SEQUENCE %s_id_seq" % (current_table))
                     sequence_lines.append("SELECT setval('%s_id_seq', max(%s)) FROM %s" % (current_table, name, current_table))
